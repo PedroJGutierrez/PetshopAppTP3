94bfb2520d17c9b092a1b024d34b8f6a2ec916e3
Juan Martin <juanmartinfrick@gmail.com>
2025-06-01 22:16:03 -0300

    Added the viewmodel

--- app/src/main/java/com/proyecto/profile/SellerProfileViewModel.kt
+++ app/src/main/java/com/proyecto/profile/SellerProfileViewModel.kt
@@ -1,5 +1,11 @@
-@HiltViewModel
-class SellerProfileViewModel @Inject constructor() : ViewModel() {
+package com.petlovers.petshopapp.ui.sellerProfile
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import com.petlovers.petshopapp.model.User
+
+class SellerProfileViewModel : ViewModel() {
     private val _user = MutableStateFlow(User("Abduldul", 80, 109, 992))
     val user: StateFlow<User> = _user
 }
